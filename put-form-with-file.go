a073acf1ed22321ce49990482ff5ea2f8659a49a
funnythingz <funnythingz69+github@gmail.com>
2015-06-26 09:48:30 +0900

    Fixed

--- put-form-with-file.go
+++ put-form-with-file.go
@@ -5,7 +5,7 @@ import (
 )
 
 func PutFormWithFile(targetUrl string, valueParams map[string]string, keyName string, filename string) (*http.Response, error) {
-	contentType, bodyBuf, err := h.CreateContentTypeAndBody(valueParams, keyName, filename)
+	contentType, bodyBuf, err := CreateContentTypeAndBody(valueParams, keyName, filename)
 	if err != nil {
 		return nil, err
 	}
